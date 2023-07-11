03069251f3f5f5f00b5b55dc757efc1b6db82f6a
Calvin <calvinducharme@gmail.com>
2023-07-10 22:44:39 -0700

    handle any video dimensions

--- main.py
+++ main.py
@@ -19,19 +19,33 @@ print("probe:", duration)
 # Get screenshot 10% into the video
 screenshot_time = duration * 0.10
 screenshot_file = "temp_screenshot.png"
-subprocess.call(['ffmpeg', '-t', str(duration), '-i', latest_file, '-vf', 'scale=1920:-1', '-frames:v', '1', screenshot_file])
+
+# Get video resolution
+probe = subprocess.check_output(['ffprobe', '-v', 'error', '-show_entries', 'stream=width,height', '-of', 'default=noprint_wrappers=1:nokey=1', latest_file])
+video_width, video_height = map(int, probe.decode().split('\n')[:2])
+
+
+subprocess.call(['ffmpeg', '-t', str(duration), '-i', latest_file, '-vf', f'scale={video_width}:{video_height}', '-frames:v', '1', screenshot_file])
 
 result = create_screen_rectangle(screenshot_file)  # assuming you modified the function to accept an image file
 
 output_file = os.path.splitext(latest_file)[0] + "_cropped.mp4"
 output_gif = os.path.splitext(latest_file)[0] + "_cropped.gif"
 
-subprocess.call(['ffmpeg', '-i', latest_file, '-vf', f"crop={result[2]}:{result[3]}:{result[0]}:{result[1]}", output_file])
+x, y, w, h = result
+print(x, y, w, h)
+
+if x + w > video_width:
+    w = video_width - x
+if y + h > video_height:
+    h = video_height - y
+
+subprocess.call(['ffmpeg', '-i', latest_file, '-vf', f"crop={w}:{h}:{x}:{y}", output_file])
 subprocess.call(['ffmpeg', '-i', output_file, '-vf', 'fps=10,scale=320:-1:flags=lanczos,palettegen', '-y', 'palette.png'])
 subprocess.call(['ffmpeg', '-i', output_file, '-i', 'palette.png', '-filter_complex', 'fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse', '-y', output_gif])
 
 
-send2trash(latest_file)
+# send2trash(latest_file)
 send2trash(output_file)
-os.remove(screenshot_file) 
+# os.remove(screenshot_file) 
 os.remove("palette.png")
